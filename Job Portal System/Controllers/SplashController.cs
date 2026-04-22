efb3cb78b72447b819614894ef9abc3d34a6950d
Mehwish Zehra <mehwishzehra12345@gmail.com>
2026-04-22 20:22:19 +0500

    sample web.config

---
+++ Job Portal System/Controllers/SplashController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Portal_System.Controllers
+{
+    public class SplashController : Controller
+    {
+        // GET: Splash
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
